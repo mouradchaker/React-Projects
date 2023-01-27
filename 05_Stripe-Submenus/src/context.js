6a314d3f97fa80d2afe5e51d8fedca1c7a56c2d8
mouradchaker <113807363+mouradchaker@users.noreply.github.com>
2023-01-27 23:19:07 +0100

    Hero and Sidebar

--- 05_Stripe-Submenus/src/context.js
+++ 05_Stripe-Submenus/src/context.js
@@ -4,7 +4,7 @@ import sublinks from "./data";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
 
   const openSidebar = () => {
